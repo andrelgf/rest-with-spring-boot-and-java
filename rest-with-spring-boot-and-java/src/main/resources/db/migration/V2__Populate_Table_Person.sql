166f47810f1ef561bd7c2c4bdf7a0fbcce9cc5f7
André Ferro <andrelgf97@gmail.com>
2023-10-02 10:05:06 -0300

    flyway db migrations

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,12 @@
+INSERT INTO `person` (`first_name`, `last_name`, `address`, `gender`)
+VALUES
+  ('John', 'Doe', '123 Main St', 'Male'),
+  ('Jane', 'Smith', '456 Elm St', 'Female'),
+  ('Michael', 'Johnson', '789 Oak St', 'Male'),
+  ('Emily', 'Brown', '101 Pine St', 'Female'),
+  ('David', 'Wilson', '222 Maple St', 'Male'),
+  ('Sarah', 'Davis', '333 Birch St', 'Female'),
+  ('Robert', 'Taylor', '444 Cedar St', 'Male'),
+  ('Jennifer', 'Lee', '555 Redwood St', 'Female'),
+  ('William', 'Clark', '666 Sequoia St', 'Male'),
+  ('Jessica', 'Anderson', '777 Spruce St', 'Female');
